10731eb35edc151342b28c0ad840e082fdb5275d
Raquel Marangoni <rmarangonisan@gmail.com>
2024-09-28 20:04:08 -0300

    documentation: add compound deviation class

---
+++ variáveis/desvio-composto.py
@@ -0,0 +1,7 @@
+# Pedir a idade do aluno
+idade = int(input("Por favor, informe a idade do aluno: "))
+# Verificar se o aluno é maior de idade ou menor de idade
+if idade >= 18:
+    print("O aluno é maior de idade")
+else:
+    print("O aluno é menor de idade")
