3a1df667e478aa53947e695cfd458ddc1b084084
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-01 23:16:30 -0300

    documentation: exercises list

---
+++ exercícios/exercicio-3.py
@@ -0,0 +1,7 @@
+# 5 colaboradores foram sorteados para ganhar um console de última geração, cada um. Porém a empresa 
+# pede que todos os membros recebam o mesmo aparelho. Crie um algoritmo onde o usuário possa digitar o 
+# voto de cada um dos 5 membros da equipe e, ao final, exiba o console escolhido e com quantos votos
+# As opções são: PLAYSTATION, XBOX e NINTENDO.
+
+escolherConsole = int(input("Informe qual console você quer receber: \n1-PLAYSTATION \n2-XBOX \n3-NINTENDO \n: "))
+
