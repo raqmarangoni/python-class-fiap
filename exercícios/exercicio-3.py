800138ccd703a93904ad2579b42a65d79e26a9de
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-03 22:50:10 -0300

    fix: answer exercise 3

--- exercícios/exercicio-3.py
+++ exercícios/exercicio-3.py
@@ -3,5 +3,30 @@
 # voto de cada um dos 5 membros da equipe e, ao final, exiba o console escolhido e com quantos votos
 # As opções são: PLAYSTATION, XBOX e NINTENDO.
 
-escolherConsole = int(input("Informe qual console você quer receber: \n1-PLAYSTATION \n2-XBOX \n3-NINTENDO \n: "))
+voto1 = input("Informe qual console você quer receber: PLAYSTATION, XBOX ou NINTENDO: ")
+voto2 = input("Informe qual console você quer receber: PLAYSTATION, XBOX ou NINTENDO: ")
+voto3 = input("Informe qual console você quer receber: PLAYSTATION, XBOX ou NINTENDO: ")
+voto4 = input("Informe qual console você quer receber: PLAYSTATION, XBOX ou NINTENDO: ")
+voto5 = input("Informe qual console você quer receber: PLAYSTATION, XBOX ou NINTENDO: ")
 
+playstation = 0
+xbox = 0
+nintendo = 0
+
+if voto5.upper() == "PLAYSTATION":
+    playstation = playstation + 1
+elif voto5.upper() == "XBOX":
+    xbox = xbox + 1
+elif voto5.upper() == "NINTENDO":
+    nintendo = nintendo + 1
+else:
+    print("O console digitado é inexistente, o voto será desconsiderado")
+
+if playstation > xbox and playstation > nintendo:
+    print("O console escolhido foi playstation")
+elif xbox > playstation and xbox > nintendo:
+    print("O console escolhido foi xbox")
+elif nintendo > playstation and nintendo > xbox:
+    print("O console escolhido foi nintendo")
+else:
+    print("Houve um empate")
