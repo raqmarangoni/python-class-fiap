39b4a11fec294f1075b574da7bf6b2076b9ddf77
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-12 20:36:36 -0300

    documentation: add list introduction class

---
+++ listas/introducao-lista.py
@@ -0,0 +1,7 @@
+listaDeInstrumentos = ["Piano", "Guitarra", "Bateria"]
+
+print(type(listaDeInstrumentos)) # <class 'list'>
+print(listaDeInstrumentos[0]) # Piano
+# Um exemplo com loop for para exibir todos os instrumentos da lista:
+for instrumento in listaDeInstrumentos:
+    print(instrumento) # Piano Guitarra Bateria
