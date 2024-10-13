0f36d61bc5fa0aa55440ad47dc71be5e62453148
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-12 23:35:47 -0300

    documentation: add introductory class to functions

---
+++ funções/introducao.py
@@ -0,0 +1,13 @@
+def somar(): # a palavra def é de definição: ela dá um nome para alguma tarefa, neste caso o nome é somar
+    valor1 = int(input("Digite um valor: "))
+    valor2 = int(input("Digite outro valor: "))
+    soma = valor1 + valor2
+    print(soma)
+
+# Executar a função:
+somar() 
+
+# Terminal:
+# Digite um valor: 1
+# Digite outro valor: 2
+# 3 //
