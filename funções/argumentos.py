0dbaf194a515fbbd22dc0d7b2b709c779b899c2e
Raquel Marangoni <rmarangonisan@gmail.com>
2024-10-12 23:44:09 -0300

    documentation: add argumetns class to functions

---
+++ funções/argumentos.py
@@ -0,0 +1,11 @@
+# Usando o exemplo da aula anterior, vamos utilizar argumentos na função
+def somar(valor1, valor2 ): # A função precisa receber dois valores como argumentos
+    soma = valor1 + valor2
+    print(soma)
+# Executar a função passando dois valores:
+somar(2, 3) # 5
+
+# Terminal:
+# Digite um valor: 1
+# Digite outro valor: 2
+# 3 //
